11e8c49a1953e532a24fd949803dc22d02a723b0
A-Guldborg <aguh@itu.dk>
2022-09-23 14:12:34 +0200

    Supress nullable and non-nullable warnings with default values

--- Assignment3.Entities/Tag.cs
+++ Assignment3.Entities/Tag.cs
@@ -3,7 +3,7 @@ namespace Assignment3.Entities;
 public class Tag
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public Task[] Tasks { get; set; }
+    public string Name { get; set; } = "sys_auto";
+    public Task[] Tasks { get; set; } = new Task[0];
     
 }
--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -3,7 +3,7 @@ namespace Assignment3.Entities;
 public class User
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public Task[] Tasks { get; set; }
+    public string Name { get; set; } = "John Doe";
+    public string Email { get; set; } = "JohnDoe@gmail.com";
+    public Task[] Tasks { get; set; } = new Task[0];
 }
