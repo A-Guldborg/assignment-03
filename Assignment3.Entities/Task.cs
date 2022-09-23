9d035f26087b02984d76d551cab5a9040b002a06
William Skou Heidemann <wihe@itu.dk>
2022-09-23 14:29:01 +0200

    implement kanbancontext

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -1,12 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment3.Entities;
 
 public class Task
 {
     public int Id { get; set; }
-    public string Title { get; set; } = "Anonymous Task";
+    [Required] public string Title { get; set; } = "Anonymous Task";
     public User? AssignedTo { get; set; }
     public string? Description { get; set; }
-    public State State { get; set; }
+    [Required] public State State { get; set; }
     public Tag[] Tags { get; set; } = new Tag[0];
 }
 
--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment3.Entities;
 
 public class User
 {
     public int Id { get; set; }
-    public string Name { get; set; } = "John Doe";
-    public string Email { get; set; } = "JohnDoe@gmail.com";
+    [Required] public string Name { get; set; } = "John Doe";
+    [Required] public string Email { get; set; } = "JohnDoe@gmail.com";
     public Task[] Tasks { get; set; } = new Task[0];
 }
