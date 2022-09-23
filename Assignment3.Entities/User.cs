9dd5c5a1686ae50252f0cc4a3ac5b9b3f7106943
William Skou Heidemann <wihe@itu.dk>
2022-09-23 13:27:46 +0200

    Implement POCO entities

--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -2,4 +2,8 @@ namespace Assignment3.Entities;
 
 public class User
 {
+    private int Id { get; set; }
+    private string Name { get; set; }
+    private string Email { get; set; }
+    private Task[] Tasks { get; set; }
 }
