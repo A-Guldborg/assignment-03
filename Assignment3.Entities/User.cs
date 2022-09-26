6ccae63676af48fdede7aee5f5d4003e975d0203
William Skou Heidemann <wihe@itu.dk>
2022-09-26 19:41:14 +0200

    test passes

--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -7,5 +7,5 @@ public class User
     public int Id { get; set; }
     [Required] public string Name { get; set; } = "John Doe";
     [Required] public string Email { get; set; } = "JohnDoe@gmail.com";
-    public IEnumerable<Task> Tasks { get; set; } = new List<Task>();
+    public ICollection<Task> Tasks { get; set; } = new List<Task>();
 }
