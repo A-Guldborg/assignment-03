a9841127ea3c22ffce1a1057f9cc430ebdeb46d6
A-Guldborg <aguh@itu.dk>
2022-09-26 20:00:29 +0200

    MSSQL implemented

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿global using Assignment3.Entities;
+
+// var factory = new KanbanContextFactory();
+// using var context = factory.CreateDbContext(args);
+
+// var user = new User
+// {
+//     Name = "Susan Doe",
+//     Email = "Susan@Doe.com"
+// };
+
+// context.Users.Add(user);
+// context.SaveChanges();
+
+// var test = context.Users.Find(1);
+
+// Console.WriteLine(test);
+Console.WriteLine("Test");
+var factory = new KanbanContextFactory();
